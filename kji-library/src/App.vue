52ae4212953e1668bd5abe54eae69d6a9beb47c6
Kexin Ji <kjii0006@student.monash.edu>
2026-07-07 11:43:15 +0800

    Complete Lab 3 Bootstrap responsive form

--- kji-library/src/App.vue
+++ kji-library/src/App.vue
@@ -1,7 +1,14 @@
-<script setup>
+<!-- <script setup>
 import JSON from './components/JSON.vue'
 </script>
 
 <template>
   <JSON />
+</template> -->
+<template>
+  <Form />
 </template>
+
+<script setup>
+import Form from './components/Form.vue'
+</script>
