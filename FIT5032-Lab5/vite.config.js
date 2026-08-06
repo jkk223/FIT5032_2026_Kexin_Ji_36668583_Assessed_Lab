1231179658cb81c1084f766eaeaa330f04123626
Kexin Ji <kjii0006@student.monash.edu>
2026-08-06 22:01:41 +0800

    Revert "Add Lab12 GitHub Pages deployment"
    This reverts commit 11e2973b4b2d203fa599e994ec9d663566a6e72c.

--- FIT5032-Lab5/vite.config.js
+++ FIT5032-Lab5/vite.config.js
@@ -6,9 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-    ? '/FIT5032_2026_Kexin_Ji_36668583_Assessed_Lab/'
-    : '/',
   plugins: [
     vue(),
     vueDevTools(),
